fad700fbdb33b358ad93aaca99ba14c2a00ed4d7
Roger Carillo <rcarillo@stackify.com>
2019-08-22 03:41:43 +0800

    Added sample project for Scaffold-DbContext demonstration

---
+++ NetDemo.Models2/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDemo.Models2
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            Training = new HashSet<Training>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string EmailAddress { get; set; }
+        public string SecurityToken { get; set; }
+
+        public virtual ICollection<Training> Training { get; set; }
+    }
+}
---
+++ NetDemo.Models2/Training.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDemo.Models2
+{
+    public partial class Training
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? PersonId { get; set; }
+        public string Desc { get; set; }
+        public string Location { get; set; }
+
+        public virtual Person Person { get; set; }
+    }
+}
