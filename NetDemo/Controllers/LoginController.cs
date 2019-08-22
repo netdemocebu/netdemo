a19e741b096ad166e533f2b86dfb6f87bbe59138
doff <doffinyl@doffinyl.com>
2019-08-23 06:46:43 +0800

    view - login/register

---
+++ NetDemo/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Verification()
+        {
+            return View("Verification");
+        }
+    }
+}
